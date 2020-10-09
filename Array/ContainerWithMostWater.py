0f9f70715cff1090f1ab42a6a78bdfb8f25c202a
zhibaichen <zhibaichen@gmail.com>
2020-10-08 22:04:20 -0500

    Second try using two pointers. The critial condition is when to move and which pointer to move.

--- Array/ContainerWithMostWater.py
+++ Array/ContainerWithMostWater.py
@@ -4,25 +4,19 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        alist = [x for x in height]
-        alist.sort()
-        indexlist = list(range(len(alist)))
-        bestpool = 0
+        left = 0
+        right = len(height) - 1
+        bestarea = 0
+        while left != right:
+            leftht = height[left]
+            rightht = height[right]
 
-        for i in range(len(alist)-1):
-            level = alist[i]
-            cur_position = height.index(level)
-            newpool = level * max(indexlist[cur_position] - indexlist[0],
-                                  indexlist[-1] - indexlist[cur_position]
-                                  )
-            # print( 'i = ' + str(i) + '--- best = ' + str(bestpool) + '--- newpool = ' + str(newpool))
-            # print('position = ' + str(cur_position))
-            # print(indexlist)
-            if newpool > bestpool:
-                bestpool = newpool
+            newarea = min(leftht, rightht) * (right - left)
+            if newarea>bestarea:
+                bestarea = newarea
 
-            #  dequeue the current position
-            height  = height[:cur_position] + height[(cur_position+1):]
-            # dequeue the postion from index list
-            indexlist = indexlist[:cur_position] + indexlist[(cur_position+1):]
-        return bestpool
+            if leftht >= rightht:
+                right -= 1
+            else:
+                left += 1
+        return bestarea
